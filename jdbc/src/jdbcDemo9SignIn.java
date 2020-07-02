85c70925d5e9b09d1645bc4862fc321f06827812
yzh1024 <yzh1024@aliyun.com>
2020-07-02 08:59:03 +0800

    更新了jdbcDemo9signin

--- jdbc/src/jdbcDemo9SignIn.java
+++ jdbc/src/jdbcDemo9SignIn.java
@@ -19,33 +19,13 @@ public class jdbcDemo9SignIn {
         System.out.println("请输入密码");
         password = sc.next();
         boolean flag = login(name, password);
-        if(flag){
+        if (flag) {
             System.out.println("登陆成功");
-        }else{
+        } else {
             System.out.println("用户名或密码错误");
         }
 
-       /* Scanner sc = new Scanner(System.in);
-        String name;
-        String password;
-        System.out.println("请输入用户名：");
-        name = sc.next();
-        System.out.println("请输入密码");
-        password = sc.next();
-        Statement stmt = null;
-        Connection conn = null;
-        ResultSet rs = null;
 
-        try {
-            JDBCUtils.getConnection();
-            String sql = "select name from user where name ='"+name+"'" ;
-            stmt = conn.createStatement();
-            rs = stmt.executeQuery(sql);
-            System.out.println(rs.next());
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }*/
     }
 
     public static boolean login(String username, String password) {
@@ -57,22 +37,22 @@ public class jdbcDemo9SignIn {
         }
         //连接数据库判断是否登录成功
         try {
-        //1、获取连接
+            //1、获取连接
             conn = JDBCUtils.getConnection();
             //2、定义SQL
-            String sql ="select * from user where name = '"+username+"'and password = '"+password+"'";
+            String sql = "select * from user where name = '" + username + "'and password = '" + password + "'";
             //3、获取执行SQL对象
-             stmt = conn.createStatement();
+            stmt = conn.createStatement();
             //4、执行查询
             rs = stmt.executeQuery(sql);
             //5、判断
             return rs.next();
         } catch (SQLException e) {
             e.printStackTrace();
-        }finally {
-            JDBCUtils.close(rs,stmt,conn);
+        } finally {
+            JDBCUtils.close(rs, stmt, conn);
         }
 
-        return true;
+        return false;
     }
 }
